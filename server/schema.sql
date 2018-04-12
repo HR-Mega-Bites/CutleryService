0ec8c97b4f175336f92b2bd12a73c14b7073fe46
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-11 20:47:57 -0700

    Create sql schema

---
+++ server/schema.sql
@@ -0,0 +1,24 @@
+DROP TABLE recipies;
+DROP TABLE tools;
+DROP TABLE recipe_tool;
+
+CREATE TABLE recipies (
+    id integer NOT NULL PRIMARY KEY,
+    name varchar(255)
+);
+
+CREATE TABLE tools(
+    id integer NOT NULL PRIMARY KEY,
+    name varchar(255),
+    description varchar(1024),
+    manufacturer varchar(255),
+    price numeric check (price >= 0),
+    imageUrls varchar(1024)[]
+);
+
+CREATE TABLE recipe_tool( 
+    joinId integer NOT NULL PRIMARY KEY,
+    id_recipe integer CONSTRAINT rec_tool_rec_ref REFERENCES recipies (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    id_tool integer CONSTRAINT rec_tool_tool_ref REFERENCES tools (id) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
