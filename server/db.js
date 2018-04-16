237138a51a2d0c8ed7ce0ee39a16225843aa4762
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-16 11:29:43 -0700

    Update export to no longer be conditional, now use string literals for multiline queries, add post api

--- server/db.js
+++ server/db.js
@@ -6,17 +6,27 @@ const client = new Client({
 });
 
 client.connect()
-  .then(() => { module.exports.client = client; })
   .catch(err => console.log(err));
 
 const getRecipeTools = function (id) {
   const query = {
-    text: 'SELECT tools.id, tools.name, tools.description, tools.manufacturer, tools.price, tools.imageURLs ' +
-          'FROM tools INNER JOIN recipe_tool ON tools.id = recipe_tool.id_tool where recipe_tool.id_recipe = $1;',
+    text: `SELECT tools.id, tools.name, tools.description, tools.manufacturer, tools.price, tools.imageURLs
+          FROM tools INNER JOIN recipe_tool ON tools.id = recipe_tool.id_tool where recipe_tool.id_recipe = $1;`,
     values: [id],
   };
   return client.query(query);
 } 
-module.exports.getRecipeTools = getRecipeTools;
 
-// module.exports.post = postTool;
+const addTool = function({id, name, description, manufacturer, price, imageUrls}){
+  imageUrls = JSON.parse(imageUrls);
+  const query = {
+    text: 'INSERT INTO tools (id, name, description, manufacturer, price, imageUrls) VALUES ($1, $2, $3, $4, $5,$6);',
+    values: [id, name, description, manufacturer, price, imageUrls]
+  }
+  return client.query(query);
+}
+
+
+module.exports.getRecipeTools = getRecipeTools;
+module.exports.client = client;
+module.exports.addTool = addTool;
