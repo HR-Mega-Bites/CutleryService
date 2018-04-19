c8e9bd79e7466fb77b948fe22b507b7a5101cc4c
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-18 18:18:01 -0700

    add dynamic data support

--- client/components/ProductImages.jsx
+++ client/components/ProductImages.jsx
@@ -6,13 +6,13 @@ export const ProductImages = ({ images }) =>
   <Grid fluid>
     <Row className="show-grid">
       <Col md={12} className= "no-gutter">
-        <Image src={images[0]} responsive />
+        <Image src={images.length ? images[0] : null} responsive />
       </Col>
     </Row>
     <Row>
       {images.map(image =>
         (
-          <Col md={3} className="no-gutter">
+          <Col md={3} sm={6} xs= {6} className="no-gutter">
             <Image src={image} responsive />
           </Col>
         )
