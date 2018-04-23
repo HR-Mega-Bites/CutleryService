0f1368caf0568218f7841d90aa519eb52a51d433
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-23 16:41:24 -0700

    refactor adaptor location

--- test/client.test.js
+++ test/client.test.js
@@ -1,11 +1,8 @@
 const { shallow, render } = require('enzyme');
 const axios = require('axios');
 const React = require('react');
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import renderer from 'react-test-renderer';
 
-configure({ adapter: new Adapter() });
 const TestData = [
   {
     "id": 52,
